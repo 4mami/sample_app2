f4c1f91d2097c7a4877c6578910907d0a87414c7
Seya_1997 <76460459+4mami@users.noreply.github.com>
2021-01-02 17:03:47 +0900

    Add user login

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/apple', to: 'static_pages#apple'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
